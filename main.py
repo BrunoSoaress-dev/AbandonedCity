8d8240845e8023e46c0ff8555502c7c9737f36aa
Bruno <contato.brunosoaress@gmail.com>
2026-03-15 14:32:07 -0300

    Creating Menu

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-import sys
+from code.Game import Game
 
-import pygame
-
-print('Setup Start')
-pygame.init()
-window = pygame.display.set_mode(size = (600,480))
-print('Setup End')
-
-print('Loop Start')
-while True:
-    # Check for all events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit() #Close Window
-            quit() #End pygame
-
+game = Game()
+game.run()
